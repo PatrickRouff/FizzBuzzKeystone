7ffcb12cee6044f10f8a448d5abb65d37d531b87
PatrickRouff <39799526+PatrickRouff@users.noreply.github.com>
2018-05-31 07:43:43 -0500

    Update fizzbuzz.cpp

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 int main() {
-int i = 0;
-i+++;
-while (i<101) {
-  if ((i%3) == 0){
-    cout<<"Fizz";
-  } else {
-    cout<<i;
-  }
-  cout<<", ";
-  i++;
-}
+	int i = 0;
+	i++;
+	while (i<101) {
+  		if ((i%3) == 0){
+    		cout<<"Fizz";
+  	} else {
+    	cout<<i;
+  	}
+  	cout<<", ";
+  	i++;
+	}
 }
